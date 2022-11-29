aae1438ab891b1f5a7b46e67e30e25f2a7cc8b4e
mekatama <tamatte@hotmail.com>
2022-11-29 23:38:41 +0900

    基本部分の継承

---
+++ FloatingText_my1.cs
@@ -0,0 +1,9 @@
+//using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using MoreMountains.Tools;
+
+namespace MoreMountains.CorgiEngine{
+public class FloatingText_my1 : MMFeedbackCorgiEngineFloatingText{
+}
+}
